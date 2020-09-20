3fd015e63aafbd5b511f96d419a566fa066c6d8b
Jack He <jackhe@Jacks-MacBook-Pro.local>
2020-09-19 17:24:09 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import TestAnimations from './components/test-animations'
+
+function App() {
+  return (
+    <TestAnimations />
+  );
+}
+
+export default App;

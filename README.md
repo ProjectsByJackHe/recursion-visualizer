923d3384cdbd09a0edcabeb4b0536d19696dfd94
Jack He <jackhe@Jacks-MacBook-Pro.local>
2020-09-19 17:36:28 -0700

    added remoteCodeRunner

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is the backend remote code runner for my Recursion Visualizer project. 
+
+The main responsibility here is to inject custom code into whatever the user wrote and
+track the recursive call structure and send that back to the frontend to visualize the
+end result. 
---
+++ remoteCodeRunner.py
@@ -0,0 +1,36 @@
+import sys 
+import subprocess
+import injectcode as ij 
+
+originalStdout = sys.stdout
+
+# On the frontend, we will parse the input to get the 
+# - function name 
+# - single function call
+#       - we will truncate this function call line and add it as a seperate parameter
+
+
+# All the inputs we will collect from the frontend:
+# inputCode here is truncated to exclude the single function call. 
+inputCode = """
+def foo(x): 
+    if x == 0: 
+        return 0 
+    return foo(x - 1)
+"""
+inputFunctionName = "foo"
+inputFunctionCall = "foo(28)"
+
+readyToExe = ij.injectCode(inputCode, inputFunctionName, inputFunctionCall)
+
+with open("run.py", "w") as p: 
+    sys.stdout = p 
+    print(readyToExe) # print in this instance writes to file instead of outputting text
+    sys.stdout = originalStdout
+
+try:
+    output = str(subprocess.check_output(["python3", "b.py"])) 
+    output = output[3:len(output) - 4]
+    print(output)
+except: 
+    print('ERROR!')

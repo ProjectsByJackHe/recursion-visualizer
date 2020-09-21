3a20148dbe23555a04423376d6394a0f3d312e4b
ProjectsByJackHe <jackhe2345@gmail.com>
2020-09-20 17:37:41 -0700

    done frontend code parser

--- src/components/userinput.tsx
+++ src/components/userinput.tsx
@@ -3,39 +3,56 @@ import Controls from './controls'
 import CodeEditor from './code-editor'
 import { getFuncName, getFuncCall, truncateFuncCall } from '../util/utility-functions'
 
+
+
 const UserInput = () => {
-    const [code, setCode] = useState("#Include at least one function definition. Don't forget to call your function.")
+    const [code, setCode] = useState("# Include at least one recursive function. Don't forget to call your function.")
     
-    const runCode = () => {
-        console.log("going to send HTTP request now!")
-        console.log(code)
+    const runCode = async () => {
         // extract function name 
         const functionName = getFuncName(code)
-        if (functionName === undefined) {
+        if (functionName === -1) {
             alert("Make sure you have at least one function defined, and your function name is <= 8 characters.")
             return 
         }
+        console.log("function name: " + functionName)
+
         // extract function call. Result will be ['call', [start index, end index]]
         const functionCall = getFuncCall(code, functionName)
-        if (functionCall === undefined) {
+        if (functionCall === -1) {
             alert("Make sure you call your function") 
             return 
         }
+        console.log("function call: " + functionCall)
+
         // truncate function call 
-        const codeToSend = truncateFuncCall(code, functionCall[1])
+        const codeToSend = truncateFuncCall(code, functionCall)
+        console.log("code to send: " + codeToSend)
 
         let callTrace;
 
         try {
             // send HTTP request to flask server and store 
             // response inside callTrace
+            const domain = process.env.BACKEND || "http://localhost:5000"
+            const options = `/execute?funcName=${functionName}&funcCall=${functionCall[0]}`
+            const fetchConfig = {
+                method: "POST",
+                body: codeToSend
+            }
+            let response = await fetch(domain + options, fetchConfig)
+            callTrace  = await response.text()
         } catch (e) {
             // if and when there is some error executing the python code on the server, 
             // the flask server will send back some sort of error response. We will 
             // catch that case here and alert the user accordingly
+            alert("There was an error executing your code. Details: " + e)
         }
-
-        // render callTrace
+        
+        if (callTrace) {
+            // render callTrace
+            console.log(callTrace)
+        }
     }
     
     return <Fragment>
--- src/util/utility-functions.js
+++ src/util/utility-functions.js
@@ -1,25 +1,117 @@
+// getFuncName helper
+const findDef = (s, code) => {
+    const lookFor = "fed"
+    for (let i = 0; i < lookFor.length; i++) {
+        if (!(s - i >= 0 && lookFor[i] === code[s - i])) {
+            return false
+        }
+    }
+    // we know now that s is the starting point for 'f' in 'def'. 
+    let functionName = ""
+    for (let j = s + 1; j < code.length; j++) {
+        if (code[j] === "(") {
+            break
+        }
+        if (code[j] !== " ") {
+            functionName += code[j]
+        } 
+    }
+    return functionName
+}
+/**
+ * @param {string} code 
+ */
 export function getFuncName(code) {
     // search for 'def' and get whatever text comes after ' ' and ends at '('
     // if no def was found or if functionName is > 8 characters long, then return undefined. 
-    return code 
+    for (let i = code.length - 1; i >= 0; i--) {
+        // parse backwards to find the word 'def' 
+        let functionName = findDef(i, code)
+        if (functionName !== false) {
+            return functionName
+        }
+    }
+    return -1
 }
 
+
+// getFuncCall helper
+const reverseString = (str) => {
+    let ans = ""
+    for (let c of str) {
+        ans = c + ans
+    }
+    return ans
+}
+// getFuncCall helper
+const findCall = (s, code, functionName) => {
+    const lookFor = functionName
+    for (let i = 0; i < lookFor.length; i++) {
+        if (!(s - i >= 0 && lookFor[i] === code[s - i])) {
+            return false
+        }
+    }
+    // we know that at position s is the last character for functionName
+    // check that there is no 'def' a few characters later. 
+    let check1 = s - lookFor.length - 1 >= 0 && code[s - lookFor.length - 1] === "f"
+    let check2 = s - lookFor.length - 2 >= 0 && code[s - lookFor.length - 2] === "e"
+    let check3 = s - lookFor.length - 3 >= 0 && code[s - lookFor.length - 3] === "d"
+    if (check1 && check2 && check3) {
+        return false
+    }
+    let functionCall = ""
+    for (let k = s - lookFor.length; k < code.length; k++) {
+        if (code[k] !== ")") {
+            functionCall += code[k]
+        } else {
+            functionCall += ")"
+            return functionCall
+        }
+    }
+    return false
+}
+/**
+ * @param {string} code 
+ * @param {string} functionName 
+ */
 export function getFuncCall(code, functionName) {
     // parse code in reverse, and actively search for functionName. 
     // if found, remember index i, and go backwards some more to look for 'fed', 
     // not counting spaces. If we encounter 3 foreign characters that are not 'fed', we 
     // are good. 
     // parse forward from i and collect the entire function call, ending at ")".
-    // if no ")" was found, then return undefined. 
+    // if no ")" was found, then return -1. 
     // once entire function call is collected, store i, and store i + functionCall.length 
     // return ['call', [i, i + functionCall.length]]
 
-    return code
+    for (let i = code.length - 1; i >= 0; i--) {
+        let call = findCall(i, code, reverseString(functionName))
+        if (call) {
+            return [call, [i - functionName.length, i + (call.length - functionName.length)]]
+        }
+    }
+    return -1
 }
 
-export function truncateFuncCall(code, callLocation) {
+
+
+/**
+ * 
+ * @param {string} code 
+ * @param { -1 | (string | number[])[]} callLocation 
+ */
+export function truncateFuncCall(code, functionCall) {
     // completely remove functionCall from code via it's callLocation. 
     // split the entire code string from [s] to [s + functionCall.length]
     // recombine the split parts. 
-    return code 
+    const callLocation = functionCall[1]
+    let left = "" 
+    let right = ""
+    for (let i = 0; i < callLocation[0]; i++) {
+        left += code[i]
+    }
+    for (let j = callLocation[1] + 1; j < code.length; j++) {
+        right += code[j]
+    }
+    return left + right 
 }
